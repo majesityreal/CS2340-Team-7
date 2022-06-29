fbacc5d7fb39fc9933a01ef328d66282c4b8bc7b
Kevin <koolkev246@gmail.com>
2022-06-29 18:16:25 -0400

    enemy spawning randomly implemented

--- Assets/Scripts/SurvivingTech/EnemySpawner.cs
+++ Assets/Scripts/SurvivingTech/EnemySpawner.cs
@@ -28,9 +28,26 @@ public class EnemySpawner : MonoBehaviour
     }
     void SpawnEnemy()
     {
-        Vector3 spawnPosition = new Vector3(Random.Range(-spawnArea.x, spawnArea.x), Random.Range(-spawnArea.y, spawnArea.y), 0f);
+        Vector3 spawnPosition = GenerateRandomPosition();
+        spawnPosition += targetPlayer.transform.position; // so enemy will spawn relative to player position
         GameObject newEnemy = Instantiate(enemy);
         newEnemy.transform.position = spawnPosition;
         newEnemy.GetComponent<Enemy>().setTarget(targetPlayer);
+        newEnemy.transform.parent = transform;
+    }
+
+    Vector3 GenerateRandomPosition() // this is to generate enemy off screen
+    {
+        Vector3 spawnPosition = new Vector3();
+        float maxValue = Random.value > 0.5f ? -1f : 1f;
+        if (Random.value > 0.5f) {
+            spawnPosition.x = Random.Range(-spawnArea.x, spawnArea.x);
+            spawnPosition.y = spawnArea.y * maxValue;
+        } else {
+            spawnPosition.y = Random.Range(-spawnArea.y, spawnArea.y);
+            spawnPosition.x = spawnArea.x * maxValue;
+        }
+        spawnPosition.z = 0;
+        return spawnPosition;
     }
 }
