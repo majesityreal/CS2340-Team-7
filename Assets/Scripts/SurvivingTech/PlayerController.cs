786f0d590dd34ec5f9406fe9568f0789dbc6956a
Kevin <koolkev246@gmail.com>
2022-06-29 11:29:53 -0400

    testing done, beginning changes on player

--- Assets/Scripts/SurvivingTech/PlayerController.cs
+++ Assets/Scripts/SurvivingTech/PlayerController.cs
@@ -6,7 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rgb;
     Vector3 movementVect;
-    [SerializeField] float playerSpeed = 3.0f;
+    [SerializeField] float hitpoints = 100;
+    [SerializeField] float playerSpeed = 5.0f;
+    [SerializeField] float damageReduction = 0.0f; // percentage of damage taken mitagated by armor
 
     // Start is called before the first frame update
     void Start()
@@ -36,5 +38,19 @@ public class PlayerController : MonoBehaviour
         movementVect *= playerSpeed;
         rgb.velocity = movementVect;
         
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            // reduce hitpoints by enemy's damage accounting for damage reduction
+            //hitpoints -= gameObject.GetComponent<EnemyController>().damage*(1-damageReduction);
+            if (hitpoints <= 0)
+            {
+                Destroy(gameObject);
+                Time.timeScale = 0; // use this for pause too
+            }
+        }
     }
 }
