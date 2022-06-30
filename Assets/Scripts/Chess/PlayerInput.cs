0ec9252d286504084c6fc45f91f94d32cdfb4a8f
YuanZheng <867754967@qq.com>
2022-07-01 06:10:54 +0900

    Add feature, press ESC go back to menu

--- Assets/Scripts/Chess/PlayerInput.cs
+++ Assets/Scripts/Chess/PlayerInput.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /**
  *  Author:         Zheng Yuan
@@ -27,7 +28,10 @@ public class PlayerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-            
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
     }
 
     //public void SelectPiece(GameObject piece)
