b0f0c15d5c3fd71e36b50cbf43c6e3dc47688cc9
Kevin <koolkev246@gmail.com>
2022-07-01 22:02:22 -0400

    projectile weapon implemented

--- Assets/Scripts/SurvivingTech/PlayerController.cs
+++ Assets/Scripts/SurvivingTech/PlayerController.cs
@@ -10,16 +10,23 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float currentHitpoints = 100;
     [SerializeField] float playerSpeed = 5.0f;
     [SerializeField] float damageReduction = 0.0f; // percentage of damage taken mitagated by armor, 1.0f is invincible
-    [SerializeField] float lives = 1;
+    [SerializeField] int lives = 1;
     [SerializeField] HPBarStatus HPBar;
     Vector3 initialScaleOfSprite;
+    float lastHorizontalVector;
+    float lastVerticalVector;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         rgb = GetComponent<Rigidbody2D>();
         movementVect = new Vector3();
         initialScaleOfSprite = transform.GetChild(0).localScale;
+    }
+
+    void Start() {
+        lastHorizontalVector = 0f;
+        lastVerticalVector = 0f;
     }
 
     // Update is called once per frame
@@ -32,15 +39,21 @@ public class PlayerController : MonoBehaviour
             transform.GetChild(0).localScale = new Vector3(initialScaleOfSprite.x, initialScaleOfSprite.y, initialScaleOfSprite.z);
             // localScale = new Vector3(1, 1, 1);   
             //transform.localRotation = Quaternion.Euler(0, 0, 0);
+            lastHorizontalVector = movementVect.x;
         }
         else if (movementVect.x > 0) // go right
         {
             transform.GetChild(0).localScale = new Vector3(-initialScaleOfSprite.x, initialScaleOfSprite.y, initialScaleOfSprite.z);
             //transform.localRotation = Quaternion.Euler(0, 0, 0);
+            lastHorizontalVector = movementVect.x;
             
         }
         movementVect.x = Input.GetAxis("Horizontal"); // works with arrows and WASD
         movementVect.y = Input.GetAxis("Vertical");
+        if (movementVect.y != 0)
+        {
+            lastVerticalVector = movementVect.y;
+        }
         //transform.localRotation = Quaternion.Euler(0, 0, 0);
         movementVect = Vector3.ClampMagnitude(movementVect, 1.0f) * playerSpeed;
         //movementVect *= playerSpeed;
@@ -68,4 +81,15 @@ public class PlayerController : MonoBehaviour
         }
         HPBar.HPIndicator(currentHitpoints, maxHitpoints);
     }
+
+    public float getLastHorizontalVector()
+    {
+        return lastHorizontalVector;
+        //Debug.Log("lastHorizontalVector: " + lastHorizontalVector);
+    }
+    public float getLastVerticalVector()
+    {
+        return lastVerticalVector;
+    }
+
 }
