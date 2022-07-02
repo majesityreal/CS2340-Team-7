b557a8bf258e9f6dcc77dec4d5d464e2cdbcaa8a
Yeti0224 <woojuchee@naver.com>
2022-07-02 13:41:06 -0400

    Removed ref from MovePosition

--- Assets/Scripts/Chess/ChessManager.cs
+++ Assets/Scripts/Chess/ChessManager.cs
@@ -53,7 +53,7 @@ public class ChessManager : MonoBehaviour
         board[7, 7] = new Rook(1, 7, 7);
     }
 
-    public void MovePosition(int oldX, int oldY, int newX, int newY, ref Piece[,] board)
+    public void MovePosition(int oldX, int oldY, int newX, int newY, Piece[,] board)
     {
         RecordMove(oldX, oldY, newX, newY);
 
